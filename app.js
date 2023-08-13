912a5d7c769baecfe8bd62ff2e95dcec8fa53df9
Roman Fedianovych <tb56roman@gmail.com>
2023-08-13 18:07:34 -0500

    Added middleware for success and error messages

--- app.js
+++ app.js
@@ -3,23 +3,30 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const ExpressHbs = require('express-handlebars');
+const {engine} = require('express-handlebars');
 const expressValidator = require('express-validator');
 const connectFlash = require('connect-flash');
 const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+
 const mongo = require('mongodb');
-const mongoose = require('mongoose');
+const {connection, connect} = require('mongoose');
+connect('mongodb://127.0.0.1:27017/elearn');
+connection.on('error', console.error.bind(console, 'Cannot connect to database'));
+connection.once('open', () => console.log("Connected successfully"));
+const db = connection;
+
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const classesRouter = require('./routes/classes');
 
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', new ExpressHbs({defaultLayout: 'layout'}));
+app.engine('handlebars', engine({defaultLayout: 'layout'}));
 app.set('view engine', 'handlebars');
 
 app.use(logger('dev'));
@@ -37,7 +44,9 @@ app.use(session({
 app.use(connectFlash());
 
 app.use(function (req, res, next) {
-  res.locals.messages = require('express-messages')(req, res);
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
   next();
 })
 
@@ -46,6 +55,7 @@ app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/classes', classesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
